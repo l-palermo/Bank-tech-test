a6a99540099231002c6e70031c400d9cb1d0111c
Luigi Palermo <l-palermo@hotmail.it>
2019-08-20 21:43:19 +0100

    Update account.rb

--- lib/account.rb
+++ lib/account.rb
@@ -2,9 +2,8 @@ require './lib/transaction'
 require './lib/statement'
 # class account
 class Account
-  def initialize(transaction = Transaction, statement = Statement, _balance = Balance.new)
+  def initialize(transaction = Transaction, statement = Statement)
     @balance = 0
-    # @balance = _balance
     @history = []
     @statement = statement
     @transaction = transaction
@@ -21,18 +20,7 @@ class Account
     @history << @transaction.new.debit(ammount, @balance)
   end
 
-  # Otion 2 uses balance as injected class
-  # def deposit(ammount)
-  #   @history << @transaction.new.credit(arg, @balance.plus(arg))
-  # end
-
-  # def withdrawal(ammount)
-  #   @history << @transaction.new.debit(arg, @balance.minus(arg))
-  # end
-
   def statement
     print @statement.new.create(@history)
   end
 end
-
-
